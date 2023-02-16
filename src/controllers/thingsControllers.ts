d8b7ad73c1b153b4e16fdc797fb6c72c89e047af
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-16 20:49:03 +0100

    Configure route and controller for getThingById function

--- src/controllers/thingsControllers.ts
+++ src/controllers/thingsControllers.ts
@@ -4,3 +4,11 @@ import { type Request, type Response } from "express";
 export const getThings = (req: Request, res: Response) => {
   res.status(200).json({ things });
 };
+
+export const getThingById = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const thingById = things.find((thing) => +id === thing.id);
+
+  res.status(200).json({ thingById });
+};
