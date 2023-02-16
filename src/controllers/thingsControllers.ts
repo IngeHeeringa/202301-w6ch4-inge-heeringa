9af200346596aa966d77c4fd634e480040676a44
Inge Heeringa <inge.heeringa@gmail.com>
2023-02-16 21:19:47 +0100

    Configure route and controller for deleteThingById function

--- src/controllers/thingsControllers.ts
+++ src/controllers/thingsControllers.ts
@@ -8,7 +8,21 @@ export const getThings = (req: Request, res: Response) => {
 export const getThingById = (req: Request, res: Response) => {
   const { id } = req.params;
 
-  const thingById = things.find((thing) => +id === thing.id);
+  const thingById = things.find((thing) => thing.id === +id);
 
   res.status(200).json({ thingById });
+};
+
+export const deleteThingById = (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  const thingById = things.find((thing) => thing.id === +id)!;
+
+  things.splice(things.indexOf(thingById), 1);
+
+  res.status(200).json({ things });
+};
+
+export const createTing = (req: Request, res: Response) => {
+  res.status(200).json({});
 };
--- src/routers/thingsRouters.ts
+++ src/routers/thingsRouters.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
-import { getThings, getThingById } from "../controllers/thingsControllers.js";
+import {
+  getThings,
+  getThingById,
+  deleteThingById,
+} from "../controllers/thingsControllers.js";
 
 const thingsRouter = Router();
 
 thingsRouter.get("/things", getThings);
 thingsRouter.get("/things/:id", getThingById);
+thingsRouter.delete("/things/:id", deleteThingById);
 
 export default thingsRouter;
